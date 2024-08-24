e8eefc193d41353c6c8d9df8729dcde55e93e7a7
Baibhab Adhikari <jiniaadhikari.resto@gmail.com>
2024-08-24 12:13:24 +0530

    ongoing codes and solutions

---
+++ day2/adamas_marking_system.c
@@ -0,0 +1,16 @@
+// adamas university marking system
+
+#include <stdio.h>
+
+typedef struct
+{
+    char Subject[5]; // subjects array
+    int credit;      // subject credit
+    int marks;       // overall marks out of 100
+} adamas_marking_system;
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
