79dd84b383cbcbffeb43e925383c4f3993a74aeb
Baibhab Adhikari <jiniaadhikari.resto@gmail.com>
2024-08-30 11:03:46 +0530

    day 3 codes

---
+++ day 3/shopping_list.cpp
@@ -0,0 +1,28 @@
+// shopping list implementation in a CPP class
+
+#include <iostream>
+
+#define MAX 10
+
+class Shopping
+{
+private:
+    int itemCode[MAX];
+    float itemPrice[MAX];
+
+public:
+};
+
+// Shopping::
+// {
+// }
+
+// Shopping::
+// {
+// }
+
+int main()
+{
+
+    return 0;
+}
